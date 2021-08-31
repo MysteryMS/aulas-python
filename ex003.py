945c297974950e0eda9045c44ed69e3c2176ce70
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-08-31 16:28:30 -0300

    adicionado os primeiros passos da linguagem

---
+++ ex003.py
@@ -0,0 +1,4 @@
+# input para capturar a entrada do usuário
+nome = input("Qual o seu nome: ")
+idade = input("Qual a sua idade: ")
+print ("Olá", nome, "sua idade é", idade)
---
+++ ex006.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que leia um número e
+# exiba para o usuário o quadrado e a metade desse número.
+
+numero = int(input("Entre com um número:"))
+quadrado = numero * numero
+metade = numero / 2
+
+print(f"O quadrado de {numero} é {quadrado} e a metade é {metade}")
