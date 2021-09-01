80086126c86bcd252778648bc0fd21c8681bbb58
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-01 11:41:16 -0300

    alterado os nomes das variáveis

--- exercicios/lista4-ex07.py
+++ exercicios/lista4-ex07.py
@@ -2,9 +2,9 @@
 # média final deste aluno, considerando que a média é ponderada, ou seja,
 # o peso das notas são 3 e 7. 
 
-nota1 = float(input("Digite a nota 1: "))
-nota2 = float(input("Digite a nota 2: "))
+n1 = float(input("Digite a nota 1: "))
+n2 = float(input("Digite a nota 2: "))
 
-media = (nota1 * 0.3) + ( nota2 * 0.7)
+media = (n1 * 0.3) + ( n2 * 0.7)
 
 print("A média ponderada é: ",media)
