d0188030c39e843d1f8bd5242138f01cf1412e1f
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-01 08:02:03 -0300

    alterado nome da variável

--- exercicios/lista4-ex09.py
+++ exercicios/lista4-ex09.py
@@ -2,9 +2,8 @@
 # maior que 100, deve-se exibir a mensagem “Número maior que 100!”.
 # Caso contrário, exiba a mensagem “Programa encerrado”.
 
-numero = int(input("Digite um número:"))
-
-if numero > 100:
+num = int(input("Digite um número:"))
+if num > 100:
     print("Número maior que 100!")
 else:
     print("Programa encerrado")
