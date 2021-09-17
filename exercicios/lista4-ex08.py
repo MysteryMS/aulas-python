cc89fa1fc188370853d97848e264738651a2417b
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-17 10:22:30 -0300

    adicionado material aula 22

--- exercicios/lista4-ex08.py
+++ exercicios/lista4-ex08.py
@@ -3,9 +3,9 @@
 
 numero1 = int(input("Digite o primeiro número:"))
 numero2 = int(input("Digite o segundo número:"))
+mult = numero1 * numero2
 
-if numero1 >= 50:
-    mult = numero1 * numero2
+if mult >= 50:
     print("A multiplicação dos números é: ",mult)
 
 print("Programa encerrado")
